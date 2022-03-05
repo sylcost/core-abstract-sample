87d1abaf2d9fc836ee22065ce9405a57c4001933
sylvain.costanzo <sylvain.costanzo@adevinta.com>
2022-03-05 02:05:32 +0100

    Add my core and legacy modules.

---
+++ core-legacy-sylvain/src/main/java/com/example/demo/core/ShoeCoreLegacy.java
@@ -0,0 +1,23 @@
+package com.example.demo.core;
+
+import java.util.List;
+
+import com.example.demo.dto.in.ShoeFilter;
+import com.example.demo.dto.in.ShoeFilter.Color;
+import com.example.demo.dto.out.Shoe;
+import com.example.demo.dto.out.Shoes;
+
+@Implementation(version = 1)
+public class ShoeCoreLegacy extends AbstractShoeCore {
+
+  @Override
+  public Shoes search(final ShoeFilter filter) {
+    return Shoes.builder()
+                .shoes(List.of(Shoe.builder()
+                                   .name("Legacy shoe")
+                                   .color(Color.BLUE)
+                                   .size(1L)
+                                   .build()))
+                .build();
+  }
+}
---
+++ core-new-sylvain/src/main/java/com/example/demo/core/ShoeCoreNew.java
@@ -0,0 +1,33 @@
+package com.example.demo.core;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.dto.in.ShoeFilter;
+import com.example.demo.dto.out.Shoe;
+import com.example.demo.dto.out.Shoes;
+import com.example.demo.models.ShoeEntity;
+import com.example.demo.repository.ShoeRepository;
+
+@Implementation(version = 2)
+public class ShoeCoreNew extends AbstractShoeCore {
+
+  private final ShoeRepository shoeRepository;
+
+  @Autowired
+  public ShoeCoreNew(final ShoeRepository shoeRepository) {
+    this.shoeRepository = shoeRepository;
+  }
+
+
+  @Override
+  public Shoes search(final ShoeFilter filter) {
+
+    List<ShoeEntity> l = shoeRepository.getAllByColorAndSize(filter.getColor(), filter.getSize());
+    List<Shoe> result = l.stream().map(shoeEntity -> Shoe.builder().color(shoeEntity.getColor()).size(shoeEntity.getSize()).name("new shoe").build()).toList();
+
+    return Shoes.builder()
+                .shoes(result)
+                .build();
+  }
+}
