c4046629ee519d3d81dce4cd1ee8ffbaf221de96
sylvain.costanzo <sylvain.costanzo@adevinta.com>
2022-03-05 15:51:34 +0100

    Add shop and stock tables.

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -6,3 +6,16 @@ CREATE TABLE IF NOT EXISTS shoe (
   PRIMARY KEY (shoe_id)
 );
 
+CREATE TABLE IF NOT EXISTS shop (
+  shop_id INT NOT NULL,
+  shop_name varchar(250),
+  PRIMARY KEY (shop_id)
+);
+
+CREATE TABLE IF NOT EXISTS stock (
+  shop_id INT NOT NULL REFERENCES shop (shop_id),
+  shoe_id INT NOT NULL REFERENCES shoe (shoe_id),
+  quantity INT NOT NULL DEFAULT 0,
+  PRIMARY KEY (shop_id, shoe_id) 
+);
+
--- sql/fill_tables.sql
+++ sql/fill_tables.sql
@@ -1,12 +1,22 @@
-INSERT INTO SHOE (shoe_id, color, size, quantity) values (1, 'BLACK', 40, 10);
-INSERT INTO SHOE (shoe_id, color, size, quantity) values (2, 'BLACK', 41, 11);
-INSERT INTO SHOE (shoe_id, color, size, quantity) values (3, 'BLACK', 42, 12);
-INSERT INTO SHOE (shoe_id, color, size, quantity) values (4, 'BLACK', 43, 13);
-INSERT INTO SHOE (shoe_id, color, size, quantity) values (5, 'BLUE', 40, 20);
-INSERT INTO SHOE (shoe_id, color, size, quantity) values (6, 'BLUE', 41, 21);
-INSERT INTO SHOE (shoe_id, color, size, quantity) values (7, 'BLUE', 42, 22);
-INSERT INTO SHOE (shoe_id, color, size, quantity) values (8, 'BLUE', 43, 23);
-INSERT INTO SHOE (shoe_id, color, size, quantity) values (9, 'RED', 40, 5);
-INSERT INTO SHOE (shoe_id, color, size, quantity) values (10, 'RED', 41, 6);
-INSERT INTO SHOE (shoe_id, color, size, quantity) values (11, 'RED', 42, 7);
-INSERT INTO SHOE (shoe_id, color, size, quantity) values (12, 'RED', 43, 8);
+INSERT INTO SHOE (shoe_id, color, size) VALUES (1, 'BLACK', 40);
+INSERT INTO SHOE (shoe_id, color, size) VALUES (2, 'BLACK', 41);
+INSERT INTO SHOE (shoe_id, color, size) VALUES (3, 'BLACK', 42);
+INSERT INTO SHOE (shoe_id, color, size) VALUES (4, 'BLACK', 43);
+INSERT INTO SHOE (shoe_id, color, size) VALUES (5, 'BLUE', 40);
+INSERT INTO SHOE (shoe_id, color, size) VALUES (6, 'BLUE', 41);
+INSERT INTO SHOE (shoe_id, color, size) VALUES (7, 'BLUE', 42);
+INSERT INTO SHOE (shoe_id, color, size) VALUES (8, 'BLUE', 43);
+INSERT INTO SHOE (shoe_id, color, size) VALUES (9, 'RED', 40);
+INSERT INTO SHOE (shoe_id, color, size) VALUES (10, 'RED', 41);
+INSERT INTO SHOE (shoe_id, color, size) VALUES (11, 'RED', 42);
+INSERT INTO SHOE (shoe_id, color, size) VALUES (12, 'RED', 43);
+
+INSERT INTO SHOP (shop_id, shop_name) VALUES (1, 'DECATHLON NANTES');
+INSERT INTO SHOP (shop_id, shop_name) VALUES (2, 'DECATHLON LILLE');
+
+INSERT INTO STOCK (shop_id, shoe_id, quantity) VALUES (1, 1, 10);
+INSERT INTO STOCK (shop_id, shoe_id, quantity) VALUES (1, 5, 5);
+INSERT INTO STOCK (shop_id, shoe_id, quantity) VALUES (1, 11, 9);
+INSERT INTO STOCK (shop_id, shoe_id, quantity) VALUES (2, 2, 1);
+INSERT INTO STOCK (shop_id, shoe_id, quantity) VALUES (2, 6, 0);
+INSERT INTO STOCK (shop_id, shoe_id, quantity) VALUES (2, 8, 2);
