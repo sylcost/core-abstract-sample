5fa0159acef768a93d3ccb726f75a8f9a3b72b6e
sylvain.costanzo <sylvain.costanzo@adevinta.com>
2022-03-07 21:53:42 +0100

    Plug core-legacy to db + clean pom.xml

--- core-legacy-sylvain/src/main/java/com/example/demo/core/ShoeCoreLegacy.java
+++ core-legacy-sylvain/src/main/java/com/example/demo/core/ShoeCoreLegacy.java
@@ -1,24 +1,34 @@
 package com.example.demo.core;
 
-import java.math.BigInteger;
 import java.util.List;
 
-import com.example.demo.dto.enums.Color;
+import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.dto.in.ShoeFilter;
 import com.example.demo.dto.out.Shoe;
 import com.example.demo.dto.out.Shoes;
+import com.example.demo.models.ShoeEntity;
+import com.example.demo.repository.ShoeRepository;
 
 @Implementation(version = 1)
 public class ShoeCoreLegacy extends AbstractShoeCore {
 
+  private final ShoeRepository shoeRepository;
+
+  @Autowired
+  public ShoeCoreLegacy(final ShoeRepository shoeRepository) {
+    this.shoeRepository = shoeRepository;
+  }
+
   @Override
   public Shoes search(final ShoeFilter filter) {
+    List<ShoeEntity> l = shoeRepository.getAllByColorAndSize(filter.getColor(), filter.getSize());
+    List<Shoe> result = l.stream().map(shoeEntity -> Shoe.builder()
+                                                         .color(shoeEntity.getColor())
+                                                         .size(shoeEntity.getSize())
+                                                         .name(shoeEntity.getName()).build()).toList();
+
     return Shoes.builder()
-                .shoes(List.of(Shoe.builder()
-                                   .name("Legacy shoe")
-                                   .color(Color.BLUE)
-                                   .size(BigInteger.ONE)
-                                   .build()))
+                .shoes(result)
                 .build();
   }
 }
